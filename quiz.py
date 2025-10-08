2535395778fb7122cb6cdaaf0ae2e6581fd22119
Willams Sousa <osdeving@gmail.com>
2025-10-08 16:43:32 +0000

    Oct 8, 2025, 1:43 PM

--- quiz.py
+++ quiz.py
@@ -5,12 +5,12 @@ questions = [
         'answer' : '4'
     },
     {
-        'question' : 'Quanto é 10 / 2',
+        'question' : 'Quanto é 10 / 2?',
         'options' : ['5', 'não sei', '4', 'pi', 'e'],
         'answer' : '5'
     },
     {
-        'question' : 'Qual  hipotenusa de um triângulo cujo catetos valem 3 e 2',
+        'question' : 'Qual  hipotenusa de um triângulo cujo catetos valem 3 e 2?',
         'options' : ['2', 'não sei', '4', 'pi', '5'],
         'answer' : '5'
     },
@@ -23,17 +23,23 @@ questions = [
 
 num_of_questions = len(questions)
 correct = 0
+n = 0
 
 while True:
-    n = 0
     question = questions[n]
+
+    print(question['question'])
+
+    print()
+
+    [print(opt) for opt in question['options']]
 
     answer = input('Escolha uma opção e aperte enter: ')
 
     if answer.lower() == 'q':
         break
 
-    n = num_of_questions % (n + 1) + 1
+    n = (n + 1) % num_of_questions
 
 print(f'Você acertou {correct} perguntas de {num_of_questions}!')
 
